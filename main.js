bcc274b799b67ed27b0df6da0a7289e65e96f06c
Luka <luka.uranic12@gmail.com>
2022-11-04 16:08:05 +0100

    Going through examples 2

--- main.js
+++ main.js
@@ -1,151 +1,113 @@
 import { GUI } from './lib/dat.gui.module.js'
-import { mat4 } from './lib/gl-matrix-module.js';
+import { mat2, mat4 } from './lib/gl-matrix-module.js';
 import { Application } from './Application.js'
+import { Node } from './Node.js';
+import { Renderer } from './Renderer.js';
+import { FirstPersonController } from './FirstPersonController.js';
 import { WebGL } from './WebGL.js'
 import {shaders} from './shaders.js'
 
 class App extends Application {
+
     async start() {
         const gl = this.gl;
-        this.programs = WebGL.buildPrograms(gl, shaders);
-        gl.clearColor(1, 1, 1, 1);
-        gl.enable(gl.DEPTH_TEST);
-        gl.enable(gl.CULL_FACE);
+        this.renderer = new Renderer(gl);
+
+        this.time = performance.now();
+        this.startTime = this.time;
+
+        this.root = new Node();
         
-        const vertices = new Float32Array([
-            //  positions      texcoords        index
-            -1, -1, -1,  1,      0,  0,      //   0
-            -1,  1, -1,  1,      0,  1,      //   1
-            -1, -1,  1,  1,      1,  0,      //   2
-            -1,  1,  1,  1,      1,  1,      //   3
-             1, -1,  1,  1,      2,  0,      //   4
-             1,  1,  1,  1,      2,  1,      //   5
-             1, -1, -1,  1,      3,  0,      //   6
-             1,  1, -1,  1,      3,  1,      //   7
-            -1, -1, -1,  1,      4,  0,      //   8
-            -1,  1, -1,  1,      4,  1,      //   9
-             1, -1, -1,  1,      0, -1,      //  10
-             1,  1, -1,  1,      0,  2,      //  11
-             1, -1,  1,  1,      1, -1,      //  12
-             1,  1,  1,  1,      1,  2,      //  13
+        this.camera = new Node();
+        this.camera.translation = [0, 1, 0];
+        this.camera.projection = mat4.create();
+        this.root.addChild(this.camera);
+        
+        this.controller = new FirstPersonController(this.camera, this.canvas);
+
+        this.floor = new Node();
+        this.floor.scale = [10, 1, 10];
+        this.root.addChild(this.floor);
+
+        this.cube1 = new Node();
+        this.cube2 = new Node();
+        this.cube3 = new Node();
+
+        this.root.addChild(this.cube1);
+        this.root.addChild(this.cube2);
+        this.cube2.addChild(this.cube3);
+
+        this.leftRotation = 0;
+        this.rightRotation = 0;
+
+        let [model, texture] = await Promise.all([
+            this.renderer.loadModel('./res/floor/floor.json'),
+            this.renderer.loadTexture('./res/images/grass.png', {
+                mip: true,
+                min: gl.NEAREST_MIPMAP_NEAREST,
+                mag: gl.NEAREST,
+            }),
         ]);
 
-        const indices = new Uint16Array([
-             0,  2,  1,      1,  2,  3,
-             2,  4,  3,      3,  4,  5,
-             4,  6,  5,      5,  6,  7,
-             6,  8,  7,      7,  8,  9,
-             1,  3, 11,     11,  3, 13,
-            10, 12,  0,      0, 12,  2,
+        this.floor.model = model;
+        this.floor.texture = texture;
+
+        let [model2, texture2] = await Promise.all([
+            this.renderer.loadModel('./res/cube/cube.json'),
+            this.renderer.loadTexture('./res/images/crate-diffuse.png', {
+                mip: true,
+                min: gl.NEAREST_MIPMAP_NEAREST,
+                mag: gl.NEAREST,
+            }),
         ]);
-        
-        this.vao = gl.createVertexArray();
-        gl.bindVertexArray(this.vao);
 
-        this.vertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-        this.indexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-        gl.enableVertexAttribArray(0);
-        gl.enableVertexAttribArray(1);
-        gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 24, 0);
-        gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 24, 16);
-
-        this.modelMatrix = mat4.create();
-        this.viewMatrix = mat4.create();
-        this.projectionMatrix = mat4.create();
-        this.mvpMatrix = mat4.create();
-        mat4.fromTranslation(this.viewMatrix, [ 0, 0, 5 ]);
-        this.isRotationEnabled = true;
-        this.isPerspectiveCorrect = true;
-
-        const response = await fetch('./res/images/crate-diffuse.png');
-        const blob = await response.blob();
-        const imageBitmap = await createImageBitmap(blob);
-
-        this.texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageBitmap);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-
-        this.isLinearFilter = false;
-        this.isPerspectiveCorrect = true;
-        this.textureScale = 1;
-        this.offsetX = 0;
-        this.offsetY = 0;
-        this.scaleX = 1;
-        this.scaleY = 1;
+        this.cube1.model = model2;
+        this.cube2.model = model2;
+        this.cube3.model = model2;
+        this.cube1.texture = texture2;
+        this.cube2.texture = texture2;
+        this.cube3.texture = texture2;
     }
     
     update() {
-        if (this.isRotationEnabled) {
-            const time = performance.now();
-            mat4.identity(this.modelMatrix);
-            mat4.fromScaling(this.modelMatrix, [this.scaleX, this.scaleY, 1, 1])
-            mat4.fromTranslation(this.modelMatrix, [this.offsetX, this.offsetY, 0])
-            mat4.rotateX(this.modelMatrix, this.modelMatrix, time * 0.0007);
-            mat4.rotateY(this.modelMatrix, this.modelMatrix, time * 0.0006);
-            this.updateModelViewProjection();
-        }
+        const t1 = this.cube1.localMatrix;
+        mat4.fromTranslation(t1, [-2, 1, -5]);
+        mat4.rotateX(t1, t1, this.leftRotation);
+        this.cube1.localMatrix = t1
+
+        const t2 = this.cube2.localMatrix;
+        mat4.fromTranslation(t2, [2, 1, -5]);
+        mat4.rotateX(t2, t2, this.rightRotation);
+        this.cube2.localMatrix = t2
+
+        const t3 = this.cube3.localMatrix;
+        mat4.fromTranslation(t3, [-1, 2, -3]);
+        mat4.rotateY(t3, t3, 1);
+        this.cube3.localMatrix = t3
+
+        
+        this.time = performance.now();
+        const dt = (this.time - this.startTime) * 0.001;
+        this.startTime = this.time;
+
+        this.controller.update(dt);
     }
 
     render() {
-        const gl = this.gl;
-
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        gl.bindVertexArray(this.vao);
-        // Draw the first triangle with the first program.
-        const { program, uniforms } = this.programs.simple;
-        gl.useProgram(program);
-
-        gl.uniform1i(uniforms.uPerspectiveCorrect, this.isPerspectiveCorrect);
-        gl.uniformMatrix4fv(uniforms.uModelViewProjection, false, this.mvpMatrix);
-        gl.uniform1f(uniforms.uTextureScale, this.textureScale);
-        // gl.uniform4f(uniforms.uOffset, 0.4 + this.offsetX, 0 + this.offsetY, 0, 1);
-        // gl.uniform4f(uniforms.uScale, this.scaleX, this.scaleY, 0, 1);
-        
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.uniform1i(uniforms.uTexture, 0);
-
-        gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+        this.renderer.render(this.root, this.camera)
     }
 
     resize() {
         const w = this.canvas.clientWidth;
         const h = this.canvas.clientHeight;
         const aspect = w / h;
-        const fovy = Math.PI / 3;
+        const fovy = Math.PI / 2;
         const near = 0.1;
         const far = 100;
 
-        mat4.perspective(this.projectionMatrix, fovy, aspect, near, far);
+        mat4.perspective(this.camera.projection, fovy, aspect, near, far);
     }
 
-    updateModelViewProjection() {
-        const matrix = this.mvpMatrix;
-        mat4.copy(matrix, this.modelMatrix);
-        const view = mat4.invert(mat4.create(), this.viewMatrix);
-        mat4.mul(matrix, view, matrix);
-        mat4.mul(matrix, this.projectionMatrix, matrix);
-    }
-
-    changeFilter() {
-        const gl = this.gl;
-
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-
-        const filter = this.isLinearFilter ? gl.LINEAR : gl.NEAREST;
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, filter);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, filter);
-    }
 }
 
 const canvas = document.querySelector('canvas')
@@ -154,12 +116,18 @@ await app.init()
 document.querySelector('.loader-container').remove()
 
 const gui = new GUI()
+gui.add(app.controller, 'pointerSensitivity', 0.0001, 0.01);
+gui.add(app.controller, 'maxSpeed', 0, 10);
+gui.add(app.controller, 'decay', 0, 1);
+gui.add(app.controller, 'acceleration', 1, 100);
+gui.add(app, 'leftRotation', -3, 3);
+gui.add(app, 'rightRotation', -3, 3);
 // gui.addColor(app, 'color')
-gui.add(app, 'offsetX', -10, 10);
-gui.add(app, 'offsetY', -10, 10);
-gui.add(app, 'scaleX', 0, 5);
-gui.add(app, 'scaleY', 0, 5);
-gui.add(app, 'isLinearFilter').name('Linear filtering').onChange(e => app.changeFilter());
-gui.add(app, 'isPerspectiveCorrect').name('Perspective-correct');
-gui.add(app, 'textureScale').name('Texture scale');
-gui.add(app, 'isRotationEnabled').name('Enable rotation');
+// gui.add(app, 'offsetX', -10, 10);
+// gui.add(app, 'offsetY', -10, 10);
+// gui.add(app, 'scaleX', 0, 5);
+// gui.add(app, 'scaleY', 0, 5);
+// gui.add(app, 'isLinearFilter').name('Linear filtering').onChange(e => app.changeFilter());
+// gui.add(app, 'isPerspectiveCorrect').name('Perspective-correct');
+// gui.add(app, 'textureScale').name('Texture scale');
+// gui.add(app, 'isRotationEnabled').name('Enable rotation');
--- shaders.js
+++ shaders.js
@@ -3,17 +3,12 @@ layout (location = 0) in vec4 aPosition;
 layout (location = 1) in vec2 aTexCoord;
 
 uniform mat4 uModelViewProjection;
-uniform bool uPerspectiveCorrect;
 
 out vec2 vTexCoord;
 
 void main() {
     vTexCoord = aTexCoord;
     gl_Position = uModelViewProjection * aPosition;
-
-    if (!uPerspectiveCorrect) {
-        gl_Position /= gl_Position.w;
-    }
 }
 `;
 
@@ -21,29 +16,81 @@ const fragment = `#version 300 es
 precision mediump float;
 
 uniform mediump sampler2D uTexture;
-uniform float uTextureScale;
 
 in vec2 vTexCoord;
 
 out vec4 oColor;
 
 void main() {
-    oColor = texture(uTexture, vTexCoord * uTextureScale);
+    oColor = texture(uTexture, vTexCoord);
 }
 `;
 
+export const shaders = {
+    simple: { vertex, fragment }
+};
+
+
+
+// const simpleVertex = `#version 300 es
+// layout (location = 0) in vec2 aPosition;
+
+// uniform mat2 uTransform;
+
+// void main() {
+//     gl_Position = vec4(uTransform * aPosition, 0, 1);
+// }
+// `;
+
+// const simpleFragment = `#version 300 es
+// precision mediump float;
+
+// out vec4 oColor;
+
+// void main() {
+//     oColor = vec4(1, 0, 0, 1);
+// }
+// `;
+
+// const texturedVertex = `#version 300 es
+// layout (location = 0) in vec2 aPosition;
+// layout (location = 1) in vec2 aTexCoord;
+
+// uniform mat2 uTransform;
+
+// out vec2 vTexCoord;
+
+// void main() {
+//     vTexCoord = aTexCoord;
+//     gl_Position = vec4(uTransform * aPosition, 0, 1);
+// }
+// `;
+
+// const texturedFragment = `#version 300 es
+// precision mediump float;
+
+// uniform sampler2D uTexture;
+
+// in vec2 vTexCoord;
+
+// out vec4 oColor;
+
+// void main() {
+//     oColor = texture(uTexture, vTexCoord);
+// }
+// `;
 
 // const vertex = `#version 300 es
 // layout (location = 0) in vec4 aPosition;
-// layout (location = 1) in vec4 aColor;
+// layout (location = 1) in vec2 aTexCoord;
 
 // uniform mat4 uModelViewProjection;
 // uniform bool uPerspectiveCorrect;
 
-// out vec4 vColor;
+// out vec2 vTexCoord;
 
 // void main() {
-//     vColor = aColor;
+//     vTexCoord = aTexCoord;
 //     gl_Position = uModelViewProjection * aPosition;
 
 //     if (!uPerspectiveCorrect) {
@@ -55,6 +102,69 @@ void main() {
 // const fragment = `#version 300 es
 // precision mediump float;
 
+// uniform mediump sampler2D uTexture;
+// uniform float uTextureScale;
+
+// in vec2 vTexCoord;
+
+// out vec4 oColor;
+
+// void main() {
+//     oColor = texture(uTexture, vTexCoord * uTextureScale);
+// }
+// `;
+
+
+// // const vertex = `#version 300 es
+// // layout (location = 0) in vec4 aPosition;
+// // layout (location = 1) in vec4 aColor;
+
+// // uniform mat4 uModelViewProjection;
+// // uniform bool uPerspectiveCorrect;
+
+// // out vec4 vColor;
+
+// // void main() {
+// //     vColor = aColor;
+// //     gl_Position = uModelViewProjection * aPosition;
+
+// //     if (!uPerspectiveCorrect) {
+// //         gl_Position /= gl_Position.w;
+// //     }
+// // }
+// // `;
+
+// // const fragment = `#version 300 es
+// // precision mediump float;
+
+// // in vec4 vColor;
+
+// // out vec4 oColor;
+
+// // void main() {
+// //     oColor = vColor;
+// // }
+// // `;
+
+
+
+// const firstVertex = `#version 300 es
+// uniform vec2 uOffset;
+
+// layout (location = 0) in vec2 aPosition;
+// layout (location = 1) in vec4 aColor;
+
+// out vec4 vColor;
+
+// void main() {
+//     vColor = aColor;
+//     gl_Position = vec4(aPosition + uOffset, 0, 1);
+// }
+// `;
+
+// const firstFragment = `#version 300 es
+// precision mediump float;
+
 // in vec4 vColor;
 
 // out vec4 oColor;
@@ -64,70 +174,50 @@ void main() {
 // }
 // `;
 
+// const secondVertex = `#version 300 es
+// uniform vec2 uOffset;
+// uniform vec2 uScale;
 
+// layout (location = 0) in vec2 aPosition;
+// layout (location = 1) in vec4 aColor;
 
-const firstVertex = `#version 300 es
-uniform vec2 uOffset;
+// out vec4 vColor;
 
-layout (location = 0) in vec2 aPosition;
-layout (location = 1) in vec4 aColor;
+// void main() {
+//     vColor = vec4(1) - aColor;
+//     vColor.a = 1.0;
+//     gl_Position = vec4(aPosition * uScale + uOffset, 0, 1);
+// }
+// `;
 
-out vec4 vColor;
+// const secondFragment = `#version 300 es
+// precision mediump float;
 
-void main() {
-    vColor = aColor;
-    gl_Position = vec4(aPosition + uOffset, 0, 1);
-}
-`;
+// in vec4 vColor;
 
-const firstFragment = `#version 300 es
-precision mediump float;
+// out vec4 oColor;
 
-in vec4 vColor;
+// void main() {
+//     oColor = vColor;
+// }
+// `;
 
-out vec4 oColor;
-
-void main() {
-    oColor = vColor;
-}
-`;
-
-const secondVertex = `#version 300 es
-uniform vec2 uOffset;
-uniform vec2 uScale;
-
-layout (location = 0) in vec2 aPosition;
-layout (location = 1) in vec4 aColor;
-
-out vec4 vColor;
-
-void main() {
-    vColor = vec4(1) - aColor;
-    vColor.a = 1.0;
-    gl_Position = vec4(aPosition * uScale + uOffset, 0, 1);
-}
-`;
-
-const secondFragment = `#version 300 es
-precision mediump float;
-
-in vec4 vColor;
-
-out vec4 oColor;
-
-void main() {
-    oColor = vColor;
-}
-`;
-
-export const shaders = {
-    simple: { vertex, fragment },
-    first: {
-        vertex   : firstVertex,
-        fragment : firstFragment
-    },
-    second: {
-        vertex   : secondVertex,
-        fragment : secondFragment
-    }
-};
+// export const shaders = {
+//     // simple: {
+//     //     vertex   : simpleVertex,
+//     //     fragment : simpleFragment
+//     // },
+//     textured: {
+//         vertex   : texturedVertex,
+//         fragment : texturedFragment
+//     },
+//     simple: { vertex, fragment },
+//     first: {
+//         vertex   : firstVertex,
+//         fragment : firstFragment
+//     },
+//     second: {
+//         vertex   : secondVertex,
+//         fragment : secondFragment
+//     }
+// };
